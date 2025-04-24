44b544fd124c759363c626da5643dc69317ce46b
Aditya DevOps <aditya.devops@example.com>
2025-04-24 14:11:19 -0500

    Add only main.tf and .gitignore

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+region     = var.aws_region
+access_key = var.aws_access_key
+secret_key = var.aws_secret_key
+
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-05572e392e80aee89"  
+  instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-super-619"  
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
